137c39e137844345fa89579f5d54d821e61aa95d
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-05-20 18:53:04 -0700

    Add files via upload

---
+++ Bookmarklet AP++.js
@@ -0,0 +1,1 @@
+javascript: (() => {var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Bookmarklet_AP-Plus-Plus@main/ap++.js"; document.body.appendChild(script);script.onload = function (){runAPplusplus()}})();
