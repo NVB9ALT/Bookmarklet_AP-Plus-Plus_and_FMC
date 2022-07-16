89795d17ce0a780dafbd30603d52f246c3f602b4
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-07-15 20:47:48 -0700

    Add FMC

--- Bookmarklet AP++.js
+++ Bookmarklet AP++.js
@@ -1,1 +1,4 @@
-javascript: (() => {var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Bookmarklet_AP-Plus-Plus@main/ap++.js"; document.body.appendChild(script);script.onload = function (){runAPplusplus()}})();
+javascript: (() => {
+var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Bookmarklet_AP-Plus-Plus@main/ap++.js"; document.body.appendChild(script);script.onload = function (){runAPplusplus()};
+var scriptFMC = document.createElement('script'); scriptFMC.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Bookmarklet_AP-Plus-Plus@main/fmcE.js";document.body.appendChild(scriptFMC);scriptFMC.onload = function(){runFMC()};
+})();
