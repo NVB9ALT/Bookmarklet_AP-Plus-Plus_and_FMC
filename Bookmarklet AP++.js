811a960b42bd07166628ba67537ecbabdf770092
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-07-28 15:44:48 -0700

    Update Bookmarklet AP++.js

--- Bookmarklet AP++.js
+++ Bookmarklet AP++.js
@@ -1,4 +1,3 @@
 javascript: (() => {
-var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Bookmarklet_AP-Plus-Plus@main/ap++.js"; document.body.appendChild(script);script.onload = function (){runAPplusplus()};
-var scriptFMC = document.createElement('script'); scriptFMC.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Bookmarklet_AP-Plus-Plus@main/fmcE.js";document.body.appendChild(scriptFMC);scriptFMC.onload = function(){runFMC()};
+var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Bookmarklet_AP-Plus-Plus@main/New%20AP%2B%2B.js"; document.body.appendChild(script);script.onload = function (){runAPplusplus()};
 })();
